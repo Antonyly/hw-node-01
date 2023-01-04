894e09c11e0483d0cb0920ee4d700c6b454673a2
Antonyly <antonlyzun@gmail.com>
2023-01-04 12:27:41 +0200

    +

---
+++ db/contacts.js
@@ -0,0 +1,30 @@
+// const contacts= require("./contacts");
+// const contactsPath = ;
+
+
+// TODO: задокументировать каждую функцию
+function listContacts() {
+  // ...твой код
+}
+
+// function getContactById(contactId) {
+//   // ...твой код
+// }
+
+// function removeContact(contactId) {
+//   // ...твой код
+// }
+
+// function addContact(name, email, phone) {
+//   // ...твой код
+// }
+
+// console.log("Hello world");
+
+module.exports = {
+    listContacts,
+    // getContactById,
+    // removeContact,
+    // addContact
+};
+
